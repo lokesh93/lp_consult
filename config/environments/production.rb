35649b52a85462dbff87a04b61e027e8701764cc
lokesh93 <podipireddy5@gmail.com>
2017-09-05 02:04:06 -0400

    new commiy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.default_url_options = { :host => "www.lpsoftwareconsulting.com" }
+  config.action_mailer.default_url_options = { :host => "lp-software-consulting.herokuapp.com" }
+
+  config.action_mailer.perform_deliveries = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
